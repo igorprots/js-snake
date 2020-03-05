f8dd189a8c208851a4144551fc4cbba6ae5ce35e
Igor Protsenko <protsxe51@gmail.com>
2020-03-05 12:12:33 +0200

    finish

--- script.js
+++ script.js
@@ -1,39 +1,26 @@
-let field = document.createElement('div');
-document.body.appendChild(field);
-field.classList.add('field');
+let greeting = 'Привет! Как тебя зовут?'
+let name = prompt(greeting);
 
-for (let i = 1; i < 101; i++) {
-    let excel = document.createElement('div');
-    field.appendChild(excel);
-    excel.classList.add('excel');
+
+while (name == '' || name == null) {
+    name = prompt(greeting);
 }
 
-let excel = document.getElementsByClassName('excel');
-let x = 1,
-    y = 10;
+alert(name + ' , я загадал число от 1 до 100. Попробуй отгадать его за найменьшее количество попыток. После каждой попытки я скажу "мало", "много" или "угадал"');
 
-for (let i = 0; i < excel.length; i++) {
-    if (x > 10) {
-        x = 1;
-        y--;
+let number = random(100)
+let guess = prompt('какое число я загадал?');
+let numberofGuesses = 1;
+
+while (number != guess) {
+    numberofGuesses = numberofGuesses + 1;
+    if (number > guess) {
+        guess = prompt('Мало. Попробуй еще раз');
+
+    } else {
+        guess = prompt('Много. Попробуй еще раз');
     }
-    excel[i].setAttribute('posX', x);
-    excel[i].setAttribute('posY', y);
-    x++;
 }
 
-function generateSnake() {
-    let posX = Math.round(Math.random() * (10 - 1) + 1);
-    let posY = Math.round(Math.random() * (10 - 1) + 1);
-    return [posX, posY];
-}
 
-let coordinates = generateSnake();
-let snakeBody = [document.querySelector('[posX= "' + coordinates[0] + '"] [posY = "' + coordinates[1] + '"]')];
-// for (let i = 0; i < snakeBody.length; i++) {
-//     // snakeBody[i].classList.add('snakeBody');
-// }
-// // snakeBody[0].classList.add('head');
-
-console.log(snakeBody);
-console.log(coordinates);
+alert('Ты угадал! Это число ' + number + '. Тебе понадобилось ' + numberofGuesses + ' попыток');
